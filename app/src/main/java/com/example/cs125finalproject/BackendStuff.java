8f14b79ff30bb955cb5c3d394cf566e706353a65
HaukohlR <personguy@ameritech.net>
2019-12-09 20:06:12 -0600

    Interface

--- app/src/main/java/com/example/cs125finalproject/BackendStuff.java
+++ app/src/main/java/com/example/cs125finalproject/BackendStuff.java
@@ -1,6 +1,7 @@
 package com.example.cs125finalproject;
 
 public interface BackendStuff {
+    //makes the grid
     int[][] generateGrid(int level);
     int[] calculatePointsColumns(int[][] grid);
     int[] calculatePointsRows(int[][] grid);
